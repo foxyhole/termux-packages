ec598d977da9882eabee552fe91a2c55b2b46fd9
pcuser <pcuser@example.com>
2018-09-08 13:17:11 +0800

    Make frida-python buildable on WSL

---
+++ packages/frida-python36/build.sh
@@ -0,0 +1,6 @@
+TERMUX_PKG_HOMEPAGE=https://www.frida.re/
+TERMUX_PKG_VERSION=12
+#TERMUX_PKG_BUILD_IN_PLACE=1
+TERMUX_PKG_SRCURL=https://github.com/frida/frida-python/archive/master.zip
+TERMUX_PKG_SHA256=a6bae73cf9b077e7d5716a1d786c31f4954e0a6cb399840ebd30be3f7be513c1
+
